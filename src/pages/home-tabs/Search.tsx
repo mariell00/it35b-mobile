749fe0ece6590776a616381c8d76aed24080c998
mariell00 <doydora.mariel02@gmail.com>
2026-04-21 08:56:26 +0800

    not finish search content
    Added a card component to display additional information alongside the search functionality.

--- src/pages/home-tabs/Search.tsx
+++ src/pages/home-tabs/Search.tsx
@@ -1,21 +1,58 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButtons, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonContent, IonHeader, IonItem, IonLabel, IonList, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { useEffect, useState } from 'react';
 
 const Search: React.FC = () => {
+    const games = [
+        { name: 'Pokémon Yellow' },
+        { name: 'Mega Man X' },
+        { name: 'The Legend of Zelda' },
+        { name: 'Pac-Man' },
+        { name: 'Super Mario World' }
+    ]
+
+    const [searchText, setSearchText] = useState("");
+    const [filteredGames, setFilteredGames] = useState(games);
+
+    useEffect(() => {
+        const debounce = setTimeout(() => {
+            const filtered = games.filter(game =>
+                game.name.toLowerCase().includes(searchText.toLowerCase())
+            );
+            setFilteredGames(filtered);
+        }, 400);
+
+        return () => clearTimeout(debounce);
+    }, [searchText]);
+
     return (
         <IonPage>
-                <IonHeader>
-                    <IonToolbar>
-                        <IonButtons>
-                            <IonMenuButton></IonMenuButton>
-                        </IonButtons>
-                        <IonTitle>Search</IonTitle>
-                    </IonToolbar>
-                </IonHeader>
-              <IonContent fullscreen>
+            <IonHeader>
+                <IonToolbar>
+                    <IonButtons>
+                        <IonMenuButton></IonMenuButton>
+                    </IonButtons>
+                    <IonTitle>Search</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent fullscreen>
+                 <IonCard>
+      <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png" />
+      <IonCardHeader>
+        <IonCardTitle>Card Title</IonCardTitle>
+        <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+      </IonCardHeader>
 
-              </IonContent>
+      <IonCardContent>Here's a small text description for the card content. Nothing more, nothing less.</IonCardContent>
+    </IonCard>
+                <IonList>
+                    {games.map((item, index) => (
+                        <IonItem key={index}>
+                            <IonLabel>{item.name}</IonLabel>
+                        </IonItem>
+                    ))}
+                </IonList>
+            </IonContent>
         </IonPage>
-
     );
 }
 
