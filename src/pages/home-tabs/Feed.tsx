e04d811d0c164228c4069d37e399375554bd0338
mariell00 <doydora.mariel02@gmail.com>
2026-04-21 08:55:32 +0800

    update content feed
    Refactored the component to display a list of games using IonList and IonItem.

--- src/pages/home-tabs/Feed.tsx
+++ src/pages/home-tabs/Feed.tsx
@@ -1,22 +1,52 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButtons, IonContent, IonHeader, IonItem, IonLabel, IonList, IonListHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 
-const Template: React.FC = () => {
+const Feed: React.FC = () => {
+
+    const games = [
+        {name: 'Pokémon Yellow'},
+        {name: 'Mega Man X'},
+        {name: 'The Legend of Zelda'},
+        {name: 'Pac-Man'},
+         {name: 'Super Mario World'}
+    ]
     return (
         <IonPage>
                 <IonHeader>
                     <IonToolbar>
-                        <IonButtons>
+                        <IonButtons slot="start">
                             <IonMenuButton></IonMenuButton>
                         </IonButtons>
-                        <IonTitle>Template</IonTitle>
+                <IonTitle>Feed</IonTitle>
                     </IonToolbar>
                 </IonHeader>
-              <IonContent fullscreen>
+              <IonContent className="ion-padding">
+      <IonList>
+      <IonItem>
+        <IonLabel>Pokémon Yellow</IonLabel>
+      </IonItem>
+      <IonItem>
+        <IonLabel>Mega Man X</IonLabel>
+      </IonItem>
+      <IonItem>
+        <IonLabel>The Legend of Zelda</IonLabel>
+      </IonItem>
+      <IonItem>
+        <IonLabel>Pac-Man</IonLabel>
+      </IonItem>
+      <IonItem>
+        <IonLabel>Super Mario World</IonLabel>
+      </IonItem>
+    </IonList>
 
+    {games.map((item, index) => (
+    <IonItem key={index}>
+        <IonLabel>{item.name}</IonLabel>
+    </IonItem>
+    ))}
               </IonContent>
         </IonPage>
 
     );
 }
 
-export default Template;
+export default Feed;
